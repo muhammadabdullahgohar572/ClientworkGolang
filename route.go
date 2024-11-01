52160f8e87b013d3972d7805586be5231d1ba3ce
Abdullah <muhammadabdullahgohar572@gmail.com>
2024-11-02 04:45:15 +0500

    first

---
+++ route.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func route() {
+
+
+	r:=mux.NewRouter()
+	r.HandleFunc("/test",Createuserdata).Methods("POST")
+	r.HandleFunc("/test1",sign).Methods("POST")
+	
+	r.HandleFunc("/test2",login).Methods("POST")
+	r.HandleFunc("/Decode",Decode).Methods("POST")
+
+
+	
+	log.Fatal(http.ListenAndServe(":8080", r))
+	
+	
+}
