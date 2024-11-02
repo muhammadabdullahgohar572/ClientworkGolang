767ca95bba3b2647a3dabedda7b25dc7fbf51fdf
Abdullah <muhammadabdullahgohar572@gmail.com>
2024-11-03 02:42:40 +0500

    first

--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
-//   import
-//   (
-// 	"fmt"
-// 	"math"
-// )
 func main() {
   Dbconnect()
   route()
-  // fmt.Println(math.Pi)
+  
 }
--- route.go
+++ route.go
@@ -12,12 +12,10 @@ func CORS(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "*") // Allow all origins
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
-
 		next.ServeHTTP(w, r)
 	})
 }
