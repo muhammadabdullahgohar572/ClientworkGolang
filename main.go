3a3f593e6cdbb6f37a12345f92f0c06c62b71572
Abdullah <muhammadabdullahgohar572@gmail.com>
2024-11-03 02:58:34 +0500

    first

--- main.go
+++ main.go
@@ -1,23 +1,9 @@
 package main
 
-import (
-    "net/http"
-    
-)
-
 // Your main function
 func main() {
     Dbconnect()
     route()
 }
 
-// Vercel requires an exported HTTP handler function
-func Handler(w http.ResponseWriter, r *http.Request) {
-    // Initialize your database and routes here as needed
-    Dbconnect()
-    route()
-    
-    // Simple response to confirm the handler is working
-    w.WriteHeader(http.StatusOK)
-    w.Write([]byte("Hello from Vercel!"))
-}
+
